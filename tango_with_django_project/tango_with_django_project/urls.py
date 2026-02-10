c393ade7348e044f9e23ecb5269d9595c6ae651d
“Ishaq <3011625A@student.gla.ac.uk>
2026-02-10 17:15:25 +0000

    Fifth commit: Chapter 6

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    path('rango/', include('rango.urls')),
+    path('', views.index, name='index'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
